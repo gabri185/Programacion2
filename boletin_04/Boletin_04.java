d47b2a15e48ba36f8df5c3048f7d2f8d6c0cd5d5
gdomreyes <72072246+gabri185@users.noreply.github.com>
2020-10-28 14:23:25 +0100

    Add files via upload

---
+++ boletin_04/Boletin_04.java
@@ -0,0 +1,18 @@
+
+package boletin_04;
+
+
+
+public class Boletin_04 {
+
+    
+    public static void main(String[] args) {
+        
+        Libro obxLib1 = new Libro ();
+        obxLib1.amosar();
+        
+        Libro obxLib2 = new Libro ("Tonecho","Breogán Riveiro",200,(short)2005,9.5f);
+        obxLib2.amosar();
+    }
+    
+}
