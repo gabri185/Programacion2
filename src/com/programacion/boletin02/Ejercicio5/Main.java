abc10fb4db5c44bd92dd94e06d751e2d0393bb63
Gabriel Domínguez <gdominguezreyes@danielcastelao.org>
2020-12-10 15:50:26 +0100

    Boletin 2

---
+++ src/com/programacion/boletin02/Ejercicio5/Main.java
@@ -0,0 +1,9 @@
+package com.programacion.boletin02.Ejercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+        int millasMarinas = 100;
+        int metros = millasMarinas * 1852;
+        System.out.println(millasMarinas+" millas mariñas, son "+metros+" metros.");
+    }
+}
